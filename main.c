08b9ae56f8c5b875047392303743e7a0036d19df
Suneet1008 <97741855+Suneet1008@users.noreply.github.com>
2022-02-02 20:13:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,64 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+/*C mini project on library management system*/
+struct library{
+   char bookname[50];
+   char author[50];
+   int noofpages;
+   float price;
+};
+int main(){
+   struct library lib[100];
+   char bookname[30];
+   int i,j, keepcount;
+   i=j=keepcount = 0;
+   while(j!=6){
+      printf("\n1. Add book information\n");
+      printf("2.Display book information\n");
+      printf("3. no of books in the library\n");
+      printf("4. Exit");
+      printf ("\n\nEnter one of the above : ");
+      scanf("%d",&j);
+      switch (j){
+         /* Add book */
+         case 1:
+            printf ("Enter book name = ");
+            scanf ("%s",lib[i].bookname);
+            printf ("Enter author name = ");
+            scanf ("%s",lib[i].author);
+            printf ("Enter pages = ");
+            scanf ("%d",&lib[i].noofpages);
+            printf ("Enter price = ");
+            scanf ("%f",&lib[i].price);
+            keepcount++;
+            i++;
+            break;
+         case 2:
+            printf("you have entered the following information\n");
+            for(i=0; i<keepcount; i++){
+               printf ("book name = %s\n",lib[i].bookname);
+               printf ("\t author name = %s\n",lib[i].author);
+               printf ("\t pages = %d\n",lib[i].noofpages);
+               printf ("\t price = %f\n",lib[i].price);
+            }
+            break;
+         case 3:
+            printf("\n No of books in library : %d", keepcount);
+            break;
+         case 4:
+            exit (0);
+      }
+   }
+   return 0;
+}
